59af4dc481be197213d4eb825862089c492bbb8c
saif <warsivorlox@gmail.com>
2026-08-01 12:43:56 +0530

    loading and error state added

--- emp-erp/src/features/dashboard/services/dashboardService.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "https://dummyjson.com";
-
-export async function getEmployees() {
-  const response = await axios.get(`${BASE_URL}/users`);
-
-  return response.data;
-}
--- emp-erp/src/features/employees/pages/Employees.tsx
+++ emp-erp/src/features/employees/pages/Employees.tsx
@@ -1,31 +1,46 @@
 import { useState, useEffect } from "react";
-import { getEmployees } from "@/features/dashboard/services/dashboardService";
+import { getEmployees } from "../services/employeeService";
 import type { Employee } from "../types/employee";
 
 function Employees() {
   const [employees, setEmployees] = useState<Employee[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     loadEmployees();
-  });
+  }, []);
 
   async function loadEmployees() {
     try {
+      setLoading(true);
+      setError("");
+
       const response = await getEmployees();
 
       setEmployees(response.users);
 
-      console.log(response);
     } catch (error) {
+      setError("Failed to load employees");
       console.log(error);
+    } finally {
+      setLoading(false);
     }
+  }
+
+  if (loading) {
+    return <h2>Loading</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
     <>
       <h1>Employees</h1>
 
-      {employees.map((employee: Employee) => (
+      {employees.map((employee) => (
         <p key={employee.id}>{employee.firstName}</p>
       ))}
     </>
