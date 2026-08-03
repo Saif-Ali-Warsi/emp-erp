5fe524acaf74a3f160239ba62256b2e119be2407
saif <warsivorlox@gmail.com>
2026-08-03 14:58:39 +0530

    axios refactoring added

--- emp-erp/src/features/employees/services/employeeService.ts
+++ emp-erp/src/features/employees/services/employeeService.ts
@@ -1,22 +1,20 @@
-import axios from "axios";
+import api from "@/services/api"
 import type { EmployeeFormData } from "../types/employeeForm";
 
-const BASE_URL = "https://dummyjson.com";
-
 export async function getEmployees() {
-  const response = await axios.get(`${BASE_URL}/users`);
+  const response = await api.get(`/users`);
 
   return response.data;
 }
 
 export async function deleteEmployee(id: number) {
-  const response = await axios.delete(`${BASE_URL}/users/${id}`)
+  const response = await api.delete(`/users/${id}`)
 
   return response.data;
 }
 
 export async function getEmployee(id: string) {
-  const response = await axios.get(`${BASE_URL}/users/${id}`)
+  const response = await api.get(`/users/${id}`)
 
   return response.data;
 }
@@ -26,14 +24,14 @@ export async function updateEmployee(
   data: EmployeeFormData
 ) {
 
-  const response = await axios.put(`${BASE_URL}/users/${id}`, data)
+  const response = await api.put(`/users/${id}`, data)
 
   return response.data
 
 }
 
 export async function createEmployee(data: EmployeeFormData) {
-  const response = await axios.post(`${BASE_URL}/users/add`, data)
+  const response = await api.post(`/users/add`, data)
 
   return response.data
 }
